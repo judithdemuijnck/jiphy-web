3f0dfe033d2abf25833aa9ddff7fd6e9a66d598b
judithdemuijnck <61882827+judithdemuijnck@users.noreply.github.com>
2022-10-07 14:20:39 +0100

    create copy to clipboard Button

--- src/components/Gif/Gif.js
+++ src/components/Gif/Gif.js
@@ -1,10 +1,14 @@
 import "./Gif.css"
 
 export default function Gif(props) {
+    //send reaction when copied to clipboard - fill in?
     return (
         <div className="gif-container">
             <img className="gif" src={props.gif.url} alt={`${props.gif.title}`} />
             <button style={{ color: props.isFavorite ? "red" : "black" }} onClick={() => props.handleClick(props.gif)} className="favorite-btn">&hearts;</button>
+            <button onClick={() => navigator.clipboard.writeText(props.gif.url)} className="material-symbols-outlined copy-Btn"><span>
+                content_copy
+            </span></button>
         </div>
     )
 }
